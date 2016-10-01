cfc8bf595db438d8d92eb85f1853284a3881177d
nakajijapan <pp.kupepo.gattyanmo@gmail.com>
2016-10-01 18:33:04 +0900

    delete Tests

--- Example/Tests/Tests.m
+++
@@ -1,8 +0,0 @@
-//
-//  NKJPhotoSliderControllerTests.m
-//  NKJPhotoSliderControllerTests
-//
-//  Created by nakajijapan on 04/12/2015.
-//  Copyright (c) 2014 nakajijapan. All rights reserved.
-//
-
